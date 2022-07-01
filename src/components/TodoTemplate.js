24c94942b23fd5daded9e9476b8fecd8326d6100
Eunji Kang <etothej@naver.com>
2022-07-01 23:16:28 +0900

    TodoTemplate added

---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,17 @@
+import './TodoTemplate.scss';
+
+
+const TodoTemplate = () => {
+
+  return (
+    <div className="TodoTemplate">
+      <div className="TodoTitle">To-do List</div>
+      <div className="content"></div>
+    </div>
+  )
+
+
+};
+
+
+export default TodoTemplate;
