03a3047b736840c15e342a440a18b2a5674799eb
Eunji Kang <etothej@naver.com>
2022-07-02 15:39:49 +0900

    using props

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import React from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from './components/TodoList'
+import { useState } from "react";
 
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Buy a monthly pass',
+      checked: true,
+    }, 
 
+    {
+      id: 2,
+      text: 'Send a message to Kelly',
+      checked: true,
+    }, 
 
-
+    {
+      id: 3,
+      text: "Book a restuarant for Kelly's Birthday",
+      checked: false,
+    }
+  ]);
 
 
   return (
     <>
       <TodoTemplate>
         <TodoInsert></TodoInsert>
-        <TodoList></TodoList>
+        <TodoList todos={todos}></TodoList>
       </TodoTemplate>
     </>
   );
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,16 +4,20 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 import './TodoListItem.scss';
+import cn from 'classnames';
 
 
-const TodoListItem = () => {
+const TodoListItem = ({todo}) => {
+  const {text, checked} = todo;
+
+  //const text = todo.text
+  //const checked = todo.checked --> 객체 분할 구조
+
   return (
     <div className='TodoListItem'>
-      <div className='checkbox'>
-        <MdCheckBoxOutlineBlank/>
-        <div className='text'>
-          Tasks
-        </div>
+      <div className={cn('checkbox', {checked})}>
+        {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/> }
+        <div className='text'>{text}</div>
       </div>
       <div className='remove'>
         <MdRemoveCircleOutline></MdRemoveCircleOutline>
