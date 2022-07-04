c87c0b6db2bd2c887cdb3492e0adcb938c8bc780
Eunji Kang <etothej@naver.com>
2022-07-04 13:52:35 +0900

    key(id) error fixed

--- src/App.js
+++ src/App.js
@@ -3,33 +3,32 @@ import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
 
-
-function todoExample () {
+function todoExample() {
   const array = [
     {
-      id: 1, 
-      text: 'Book a restaurant',
+      id: 1,
+      text: "Book a restaurant",
       checked: true,
     },
     {
-      id: 2, 
-      text: 'Send an email to professor',
+      id: 2,
+      text: "Send an email to professor",
       checked: false,
     },
     {
-      id: 3, 
-      text: 'Go to the grocery store',
+      id: 3,
+      text: "Go to the grocery store",
       checked: false,
     },
   ];
   return array;
 }
 
-
 function todoReducer(todos, action) {
   switch (action.type) {
     case "INSERT":
-      return todos.concat(action.todo);
+      return (todos.concat(action.todo));
+
     case "REMOVE":
       return todos.filter((todo) => todo.id !== action.id);
     case "TOGGLE":
@@ -46,31 +45,23 @@ const App = () => {
 
   const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text: text,
-        checked: false,
-      };
-      dispatch({type: 'INSERT', todo})
-    },
-    []
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text: text,
+      checked: false,
+    };
+    nextId.current += 1;
+    dispatch({ type: "INSERT", todo });
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      dispatch({type: 'REMOVE', id})
-    },
-    []
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({ type: "REMOVE", id });
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      dispatch({type: 'TOGGLE', id})
-    },
-    []
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: "TOGGLE", id });
+  }, []);
 
   return (
     <>
